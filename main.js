512558e811f8c69c0d7e31e84950d14b391637a2
wwfstrong <865475679@qq.com>
2019-07-12 16:09:09 +0800

    添加9个文件

---
+++ main.js
@@ -0,0 +1,40 @@
+let n
+初始化()
+setInterval(()=>{
+  makeLeave(getImage(n))
+    .one('transitionend', (e)=>{
+      makeEnter($(e.currentTarget))
+    })
+  makeCurrent(getImage(n+1))
+  n += 1
+},2000)
+
+function getImage(n){
+  return $(`.images > img:nth-child(${x(n)})`)
+}
+
+function x(n){
+  if(n>5){
+    n = n%5
+    if (n===0){
+      n =5
+    }
+  } // n = 1 2 3
+  return n
+}
+
+function 初始化(){
+  n = 1
+  $(`.images > img:nth-child(${n})`).addClass('current')
+    .siblings().addClass('enter')
+}
+
+function makeCurrent($node){
+  return $node.removeClass('enter leave').addClass('current')
+}
+function makeLeave($node){
+  return $node.removeClass('enter current').addClass('leave')
+}
+function makeEnter($node){
+  return $node.removeClass('leave current').addClass('enter')
+}
